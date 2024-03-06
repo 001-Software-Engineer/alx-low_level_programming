a46a3623b9d0e56195e4cbe4fa8effa753b92bbb
001-Software-Engineer <livingstonebarasa05@gmail.com>
2024-03-05 21:43:50 -0800

    task 0

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,21 @@
+
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: string to be checked
+ * @b: constant byte to be filled
+ * @n: the first bytes in memory to be filled.
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n)
+			s[i] = b;
+	}
+	return (s);
+
+
+}
